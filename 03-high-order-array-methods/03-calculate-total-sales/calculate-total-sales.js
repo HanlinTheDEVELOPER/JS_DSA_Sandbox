8bea43f955f04ed5b57b918671ff72a57e67cdd0
HanlinTheDEVELOPER <hanlinthedeveloper@gmail.com>
2024-04-15 21:21:12 +0630

    higher order array method part 1

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,9 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(productArrays, taxRate) {
+  const totalSale = productArrays.reduce((total, product) => {
+    return total + product.price * product.quantity;
+  }, 0);
+  const result = totalSale + totalSale * (taxRate / 100);
+  return parseFloat(result.toFixed(2));
+}
 
 module.exports = calculateTotalSalesWithTax;
--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,15 @@
-function highestScoringWord() {}
+function highestScoringWord(string) {
+  const alphabets = "abcdefghijklmnopqrstuvwxyz";
+  const wordArr = string.split(" ");
+  const sumArr = wordArr.map((word) =>
+    word.split("").reduce((total, letter) => {
+      return parseInt(total + alphabets.indexOf(letter) + 1);
+    }, 0)
+  );
+  const maxNum = Math.max(...sumArr);
+  const index = sumArr.indexOf(maxNum);
+
+  return wordArr[index];
+}
 
 module.exports = highestScoringWord;
