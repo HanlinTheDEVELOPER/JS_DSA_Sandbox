c404b97bdff12bc36296b84e1c7a536396879574
HanlinTheDEVELOPER <hanlinthedeveloper@gmail.com>
2024-04-16 20:58:51 +0630

    recurrsion part 1

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,13 @@
-function reverseString() {}
+function reverseString(string) {
+  if (string.length === 0) {
+    return string;
+  }
+
+  console.log(`reverseString(${string})`);
+  let result = string.slice(-1);
+  const res = result + reverseString(string.slice(0, string.length - 1));
+  console.log(res);
+  return res;
+}
 
 module.exports = reverseString;
