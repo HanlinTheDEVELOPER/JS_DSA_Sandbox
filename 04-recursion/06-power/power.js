0c2544c4989a900578671d4b11335f5267bcf62c
HanlinTheDEVELOPER <hanlinthedeveloper@gmail.com>
2024-04-17 18:44:28 +0630

    recurrsion part2

--- 04-recursion/06-power/power.js
+++ 04-recursion/06-power/power.js
@@ -1,3 +1,6 @@
-function power() {}
+function power(base, exp) {
+  if (exp === 0) return 1;
+  return base * power(base, exp - 1);
+}
 
 module.exports = power;
--- 04-recursion/08-number-range/number-range.js
+++ 04-recursion/08-number-range/number-range.js
@@ -1,3 +1,8 @@
-function numberRange() {}
+function numberRange(start, end) {
+  if (start === end) return [start];
+  const numbers = numberRange(start + 1, end);
+  numbers.unshift(start);
+  return numbers;
+}
 
 module.exports = numberRange;
